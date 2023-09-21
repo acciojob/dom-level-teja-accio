d1c9c9c4af907380cb877c8eb0aa43c19ea74885
teja-accio <128350434+teja-accio@users.noreply.github.com>
2023-09-21 21:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const levelElement = document.getElementById("level");
+let level = 1;
+
+while (false) {
+	if (levelElement.tagName ==="HTML") {
+		break;
+		
+	}
+	levelElement = levelElement.parentNode;
+	level++;
+}
+
+	
